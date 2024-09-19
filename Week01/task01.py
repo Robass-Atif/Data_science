d56992bb5a8e3c78dcb1b707023f03141b3661c5
Robass Atif <mohammadrobass@gmail.com>
2024-09-19 14:34:41 +0500

    first week

--- Week01/task01.py
+++ Week01/task01.py
@@ -1,6 +1,32 @@
 
 import pandas as pd
+import matplotlib.pyplot as plt
 
-data=pd.readfile('data.csv')
 
+
+
+
+data=pd.read_csv('students_data.csv')
+# show top four
 print(data.head())
+
+
+# # line chart
+plt.plot(data['Student Name'],data['Study Hours Per Week'])
+plt.xlabel('Name')
+plt.ylabel('Study Hours')
+plt.show()
+
+
+data1=pd.read_csv('data.csv')
+# # bar chart
+plt.bar(data1['Name'],data1['study_hours'])
+plt.xlabel('Name')
+plt.ylabel('Study Hours')
+plt.show()
+
+
+# # scatter plot
+
+plt.scatter(data1['Name'],data1['social_media'])
+plt.show()
